dd680e4217585d6945afaf07031b5ba1367b2a12
clock <1084991652clock@gmail.com>
2016-05-21 23:48:08 +0800

    添加文档的编辑

--- src/main/java/com/clock/study/activity/PhotoPreviewActivity.java
+++ src/main/java/com/clock/study/activity/PhotoPreviewActivity.java
@@ -3,8 +3,12 @@ package com.clock.study.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.clock.study.R;
@@ -12,6 +16,7 @@ import com.clock.utils.bitmap.BitmapUtils;
 import com.clock.utils.common.RuleUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * 预览图片界面
@@ -19,14 +24,14 @@ import java.io.File;
  * @author Clock
  * @since 2016-05-13
  */
-public class PhotoPreviewActivity extends AppCompatActivity {
+public class PhotoPreviewActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final static float RATIO = 0.75f;
 
-    private final static String EXTRA_IMAGE = "extra_image";
+    private final static String EXTRA_PHOTO = "extra_photo";
 
     private ImageView mPhotoPreview;
-    private File mImageFile;
+    private File mPhotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +40,45 @@ public class PhotoPreviewActivity extends AppCompatActivity {
 
         mPhotoPreview = (ImageView) findViewById(R.id.iv_preview_photo);
 
-        mImageFile = (File) getIntent().getSerializableExtra(EXTRA_IMAGE);
+        mPhotoFile = (File) getIntent().getSerializableExtra(EXTRA_PHOTO);
         int requestWidth = (int) (RuleUtils.getScreenWidth(this) * RATIO);
         int requestHeight = (int) (RuleUtils.getScreenHeight(this) * RATIO);
-        Bitmap bitmap = BitmapUtils.decodeBitmapFromFile(mImageFile, requestWidth, requestHeight);//按照屏幕宽高的3/4比例进行缩放显示
+        Bitmap bitmap = BitmapUtils.decodeBitmapFromFile(mPhotoFile, requestWidth, requestHeight);//按照屏幕宽高的3/4比例进行缩放显示
         if (bitmap != null) {
-            int degree = BitmapUtils.getBitmapDegree(mImageFile.getAbsolutePath());//检查是否有被旋转，并进行纠正
+            int degree = BitmapUtils.getBitmapDegree(mPhotoFile.getAbsolutePath());//检查是否有被旋转，并进行纠正
             if (degree != 0) {
                 bitmap = BitmapUtils.rotateBitmapByDegree(bitmap, degree);
             }
             mPhotoPreview.setImageBitmap(bitmap);
         }
+
+        findViewById(R.id.btn_display_to_gallery).setOnClickListener(this);
     }
 
     public static void preview(Activity activity, File file) {
         Intent previewIntent = new Intent(activity, PhotoPreviewActivity.class);
-        previewIntent.putExtra(EXTRA_IMAGE, file);
+        previewIntent.putExtra(EXTRA_PHOTO, file);
         activity.startActivity(previewIntent);
     }
 
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if (viewId == R.id.btn_display_to_gallery) {
+            /*Bitmap bitmap = BitmapFactory.decodeFile(mPhotoFile.getAbsolutePath());
+            File imageFile = BitmapUtils.saveToFile(bitmap, mPhotoFile.getParentFile());
+            if (imageFile != null) {
+                BitmapUtils.displayToGallery(this, imageFile);
+            }*/
+            String photoPath = mPhotoFile.getAbsolutePath();
+            // 其次把文件插入到系统图库
+            try {
+                MediaStore.Images.Media.insertImage(getContentResolver(), photoPath, mPhotoFile.getName(), null);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            // 最后通知图库更新
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + photoPath)));
+        }
+    }
 }
