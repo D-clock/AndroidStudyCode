58ce3e9efe1a40ddd53687f6f0e48b4f717db1db
clock <1084991652clock@gmail.com>
2016-07-23 21:06:15 +0800

    添加一些 Animator 动画实践代码

--- src/main/java/com/clock/study/animator/Test.java
+++
@@ -1,7 +0,0 @@
-package com.clock.study.animator;
-
-/**
- * Created by Administrator on 2016/7/22.
- */
-public class Test {
-}
--- src/test/java/com/clock/study/ExampleUnitTest.java
+++ src/test/java/com/clock/study/ExampleUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ * To work on unit tests, switch the ColorEvaluator Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test
