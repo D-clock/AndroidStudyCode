e4733fb5b5c4ad956f6bce1d9bc9d3ad585bfe6e
clock <1084991652clock@gmail.com>
2016-08-15 18:47:06 +0800

    添加夜间模式效果

--- src/main/java/com/clock/study/activity/DayNightActivity.java
+++ src/main/java/com/clock/study/activity/DayNightActivity.java
@@ -1,10 +1,19 @@
 package com.clock.study.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.clock.study.R;
 
@@ -18,10 +27,25 @@ public class DayNightActivity extends AppCompatActivity {
 
     private final static String TAG = DayNightActivity.class.getSimpleName();
 
+    private static boolean isNight = false;
+
+    private View mRootView;
+    /**
+     * 切换模式时，保存界面效果用于
+     */
+    private ImageView mTransView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (isNight) {
+            setTheme(R.style.NightTheme);
+        } else {
+            setTheme(R.style.DayTheme);
+        }
         setContentView(R.layout.activity_day_night);
+
+        initView();
 
     }
 
@@ -36,10 +60,51 @@ public class DayNightActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.day_mode) {
-
+            isNight = false;
+            recreate();
         } else if (itemId == R.id.night_mode) {
-
+            isNight = true;
+            initTransBitmap();
+            startAnimation(mTransView);
+            recreate();
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void initView() {
+        mRootView = findViewById(R.id.layout_main);
+        mTransView = (ImageView) findViewById(R.id.iv_trans);
+    }
+
+    /**
+     * 初始化起过渡效果用的Bitmap
+     */
+    private void initTransBitmap() {
+        //开启绘制缓存，让View中的内容能够转换成为Bitmap
+        mRootView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = mRootView.getDrawingCache();
+        if (bitmap != null) {
+            mTransView.setImageBitmap(bitmap);
+            mTransView.setAlpha(1f);
+            mTransView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * 启动过度用的动画效果
+     */
+    private void startAnimation(final View transView) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(transView, "alpha", 1f, 0f);
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                transView.setVisibility(View.GONE);
+            }
+        });
+        animator.setDuration(1000);
+        animator.start();
+
+    }
+
 }
